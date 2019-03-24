8190f1f14e36642ea35c58d589f1afbe4e0d4efa
Philippe Yo <poub1@neuf.fr>
2019-03-24 22:58:00 +0100

    Données service

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -9,6 +9,32 @@ export class BoardService {
   private showMenu = true;
   menuSubject = new Subject<boolean>();
 
+  private flights = [
+    {
+      id: 1,
+      name: 'Tokyo'
+    },
+    {
+      id: 2,
+      name: 'Madrid'
+    }
+  ];
+
+  private passengers = [
+    {
+      name: 'Céline Flaux',
+      flightId: 1
+    },
+    {
+      name: 'Antoine Griezemman',
+      flightId: 1
+    },
+    {
+      name: 'John Do',
+      flightId: 2
+    }
+
+  ];
   constructor() { }
 
   emitMenu() {
