bca322f6d3fb64b6a08ad2da2dddad84e2082f09
Philippe Yo <poub1@neuf.fr>
2019-03-31 16:23:36 +0200

    Ajout de l'aide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelpComponent } from './help/help.component';
+import { GuestComponent } from './guest/guest.component';
+import { TableComponent } from './table/table.component';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'board', component: BoardComponent },
+  { path: 'guest', component: GuestComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'help',  component: HelpComponent },
+  { path: '', redirectTo: '/board', pathMatch: 'full' },
+  { path: '**', component: HelpComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
